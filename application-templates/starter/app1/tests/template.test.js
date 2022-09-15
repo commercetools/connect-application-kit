15aa719ff55fc998452598072ff484d3313ff188
Rui Pedro Dias <rui.dias@commercetools.com>
2022-09-15 10:24:12 +0100

    refactor: refactored with prettier

--- application-templates/starter/app1/tests/template.test.js
+++ application-templates/starter/app1/tests/template.test.js
@@ -1,14 +1,14 @@
 const objectTemplate = {
-    name: 'object app 1',
-    type: 'template app 1'
+  name: 'object app 1',
+  type: 'template app 1',
 };
 
 describe('Test suite for Object Template 1', () => {
-    test(`Object is named 'object app 1'`, () => {
-        expect(objectTemplate.name).toBe('object app 1');
-    })
+  test(`Object is named 'object app 1'`, () => {
+    expect(objectTemplate.name).toBe('object app 1');
+  });
 
-    test(`Object is of type 'template app 1'`, () => {
-        expect(objectTemplate.type).toBe('template app 1');
-    })
-})
+  test(`Object is of type 'template app 1'`, () => {
+    expect(objectTemplate.type).toBe('template app 1');
+  });
+});
--- application-templates/starter/app2/tests/template.test.js
+++ application-templates/starter/app2/tests/template.test.js
@@ -1,14 +1,14 @@
 const objectTemplate = {
-    name: 'object app 2',
-    type: 'template app 2'
+  name: 'object app 2',
+  type: 'template app 2',
 };
 
 describe('Test suite for Object Template 2', () => {
-    test(`Object is named 'object app 2'`, () => {
-        expect(objectTemplate.name).toBe('object app 2');
-    })
+  test(`Object is named 'object app 2'`, () => {
+    expect(objectTemplate.name).toBe('object app 2');
+  });
 
-    test(`Object is of type 'template app 2'`, () => {
-        expect(objectTemplate.type).toBe('template app 2');
-    })
-})
+  test(`Object is of type 'template app 2'`, () => {
+    expect(objectTemplate.type).toBe('template app 2');
+  });
+});
