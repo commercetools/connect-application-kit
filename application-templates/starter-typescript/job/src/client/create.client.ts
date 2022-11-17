65e63673e40856b6d8473cb1bdb43a5a4cbbcc9e
Harm Meijer <harm.meijer@commercetools.com>
2022-11-17 10:58:56 +0100

    memoize api root

--- application-templates/starter-typescript/job/src/client/create.client.ts
+++ application-templates/starter-typescript/job/src/client/create.client.ts
@@ -2,15 +2,21 @@ import { createClient } from './build.client';
 
 import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
 import { readConfiguration } from '../utils/config.utils';
+import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
 
 /**
  * Create client with apiRoot
  * apiRoot can now be used to build requests to de Composable Commerce API
  */
-export const createApiRoot = () =>
-  createApiBuilderFromCtpClient(createClient()).withProjectKey({
+export const createApiRoot = ((root?: ByProjectKeyRequestBuilder) => () => {
+  if (root) {
+    return root;
+  }
+  root = createApiBuilderFromCtpClient(createClient()).withProjectKey({
     projectKey: readConfiguration().projectKey,
   });
+  return root;
+})();
 
 /**
  * Example code to get the Project details
