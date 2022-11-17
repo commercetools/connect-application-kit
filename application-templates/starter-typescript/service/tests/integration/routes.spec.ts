f464b102bc0eac572f603107a5b6ef345dbe5680
Harm Meijer <harm.meijer@commercetools.com>
2022-11-17 10:29:46 +0100

    suggest creating tests

--- application-templates/starter-typescript/service/tests/integration/routes.spec.ts
+++ application-templates/starter-typescript/service/tests/integration/routes.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-
+//@todo: create tests (see typescript job)
 describe('Testing Cart Controller', () => {
   test('POST `/service` route', async () => {
     expect(1).toBe(1);
