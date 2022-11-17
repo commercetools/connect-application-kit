e8a27c7c524cff74bd5d59db3f550cf1f386b776
Harm Meijer <harm.meijer@commercetools.com>
2022-11-17 10:38:48 +0100

    suggest testing (tdd)

--- application-templates/starter/event/tests/integration/routes.spec.js
+++ application-templates/starter/event/tests/integration/routes.spec.js
@@ -1,3 +1,5 @@
+//@todo: test this with msw (see typescipt job)
+//  make msw a shared package
 describe('Testing Event Controller', () => {
   test('POST `/event` route', async () => {
     expect(1).toBe(1);
