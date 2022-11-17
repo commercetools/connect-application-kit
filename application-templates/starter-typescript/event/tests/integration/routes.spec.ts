36ee820ab03a6cd9dd64dc1148786729f253eb03
Harm Meijer <harm.meijer@commercetools.com>
2022-11-17 10:00:23 +0100

    resolve testing

--- application-templates/starter-typescript/event/tests/integration/routes.spec.ts
+++ application-templates/starter-typescript/event/tests/integration/routes.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, test } from '@jest/globals';
 
 describe('Testing Event Controller', () => {
+  //@todo: create test see apis in connect-services
   test('POST `/event` route', async () => {
     expect(1).toBe(1);
   });
