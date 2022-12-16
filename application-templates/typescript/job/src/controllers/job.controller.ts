7f3061ddeb21586e2b040a6a47b9cce8754b2acc
Rui Pedro Dias <rui.dias@commercetools.com>
2022-12-16 15:28:03 +0000

    fix: fix minor errors in job template

--- application-templates/typescript/job/src/controllers/job.controller.ts
+++ application-templates/typescript/job/src/controllers/job.controller.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express';
-import { UpdateAction } from '@commercetools/sdk-client-v2';
 
-import { apiSuccess } from '../api/success.api';
 import CustomError from '../errors/custom.error';
 import { logger } from '../utils/logger.utils';
 import { allOrders } from '../orders/fetch.orders';
@@ -15,22 +13,11 @@ import { allOrders } from '../orders/fetch.orders';
  */
 export const post = async (_request: Request, response: Response) => {
   try {
-    const updateActions: Array<UpdateAction> = [];
-
     // Get the orders
     const limitedOrdersObject = await allOrders({ sort: ['lastModifiedAt'] });
     logger.info(`There are ${limitedOrdersObject.total} orders!`);
 
-    // Create the UpdateActions Object to return it to the client
-    const updateAction: UpdateAction = {
-      action: 'recalculate',
-      updateProductData: false,
-      totalOrders: limitedOrdersObject.total,
-    };
-
-    updateActions.push(updateAction);
-
-    apiSuccess(200, updateActions, response);
+    response.status(200).send();
   } catch (error) {
     throw new CustomError(
       500,
--- application-templates/typescript/job/src/interfaces/response.interface.ts
+++
@@ -1,9 +0,0 @@
-import { UpdateAction } from '@commercetools/sdk-client-v2';
-
-export interface ResponseInterfaceSuccess {
-  actions: Array<UpdateAction>;
-}
-
-export interface ResponseInterfaceError {
-  errors: Array<unknown>;
-}
