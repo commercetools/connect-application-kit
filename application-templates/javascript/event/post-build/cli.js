158bb56d8602999c958b38ab9b79ce7a696b901e
Rui Pedro Dias <rui.dias@commercetools.com>
2023-02-20 11:21:14 +0000

    feat: changed process.env.var name to use deployed url

--- application-templates/javascript/event/post-build/cli.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-console */
-const runPostBuild = async () => {
-  console.log(
-    `Do all commercetools or other tool configurations here with the URL= ${process.env.CT_DEPLOYMENT_URL}.`
-  );
-};
-
-runPostBuild();
---
+++ application-templates/typescript/event/post-deploy/cli.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+const runPostBuild = async () => {
+  console.log(
+    `Do all commercetools or other tool configurations here with the URL= ${process.env.CTP_APPLICATION_URL}.`
+  );
+};
+
+runPostBuild();
