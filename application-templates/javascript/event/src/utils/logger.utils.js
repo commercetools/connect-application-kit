c7c88a03fe2d03d3fec1b524339136894fe2aad5
Dario Ocles <Dario Ocles>
2023-10-20 11:29:54 +0200

    fix: connector's logger to format logs to match GCP format

--- application-templates/javascript/event/src/utils/logger.utils.js
+++ application-templates/javascript/event/src/utils/logger.utils.js
@@ -1,3 +1,13 @@
 import { createApplicationLogger } from '@commercetools-backend/loggers';
+import { LEVEL } from 'triple-beam';
 
-export const logger = createApplicationLogger();
+class AddSeverityFormatter {
+  transform(info) {
+    info['severity'] = info[LEVEL]?.toUpperCase();
+    return info;
+  }
+}
+
+export const logger = createApplicationLogger({
+  formatters: [new AddSeverityFormatter()],
+});
