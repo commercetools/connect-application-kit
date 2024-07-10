dd78e46ba3f42fd4c16b17c61666a81ed488369e
Rui Pedro Dias <rui.dias@commercetools.com>
2024-07-10 12:21:49 +0100

    feat(templates): add new asset javascript template

---
+++ application-templates/javascript/assets/.eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-undef': 'error',
+    'no-console': 'error',
+    'no-const-assign': 'error',
+  },
+  env: {
+    es6: true,
+    jest: true,
+    node: true, //adds things like process to global
+    browser: true
+  },
+};
