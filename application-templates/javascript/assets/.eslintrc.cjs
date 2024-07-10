c908b52fe1c44dac5218d98bcd74affd446353f0
Rui Pedro Dias <rui.dias@commercetools.com>
2024-07-10 12:28:13 +0100

    fix(templates): fix node version and minor fixes

--- application-templates/javascript/assets/.eslintrc.cjs
+++ application-templates/javascript/assets/.eslintrc.cjs
@@ -1,13 +1,12 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended'],
   parserOptions: {
     ecmaVersion: 2020,
+    sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'error',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'no-undef': 'error',
     'no-console': 'error',
     'no-const-assign': 'error',
