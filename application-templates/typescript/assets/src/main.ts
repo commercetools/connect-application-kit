1f4f7ab52d68e45e52a631f3fb86bb25b956f80e
Rui Pedro Dias <rui.dias@commercetools.com>
2024-07-10 10:46:47 +0100

    feat(templates): add new asset typescript template

---
+++ application-templates/typescript/assets/src/main.ts
@@ -0,0 +1,20 @@
+import commercetoolsLogo from '../static/commercetools.svg';
+
+export const alertMessage = () => {
+  alert('Hello! You are now using the Connect Asset Application template!');
+};
+
+// Get the anchor tag and set the image source
+document.addEventListener('DOMContentLoaded', () => {
+  const imgElement = document.createElement('img');
+  imgElement.src = commercetoolsLogo;
+  imgElement.className = 'logo';
+  imgElement.alt = 'CT logo';
+
+  const linkElement = document.querySelector('a');
+  if (linkElement) {
+    linkElement.appendChild(imgElement);
+  }
+});
+
+setTimeout(alertMessage, 1000);
