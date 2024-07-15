b5a56096549792cc58b2697b6d9d7f056552edf9
Rui Pedro Dias <rui.dias@commercetools.com>
2024-07-15 14:25:06 +0100

    fix(templates): fix error middleware signature for better error handling

--- application-templates/javascript/service/src/middleware/error.middleware.js
+++ application-templates/javascript/service/src/middleware/error.middleware.js
@@ -1,19 +1,17 @@
 import CustomError from '../errors/custom.error.js';
 
-/**
- * Middleware for error handling
- */
-export const errorMiddleware = (error, req, res, _next) => {
+export const errorMiddleware = (error, _req, res, _next) => {
   if (error instanceof CustomError) {
     if (typeof error.statusCode === 'number') {
       res.status(error.statusCode).json({
         message: error.message,
         errors: error.errors,
+        stack: error.stack
       });
 
       return;
     }
   }
 
-  res.status(500).send('Internal server error');
+  res.status(500).send('Internal server error: ' + error.stack);
 };
--- application-templates/typescript/job/src/middleware/error.middleware.ts
+++ application-templates/typescript/job/src/middleware/error.middleware.ts
@@ -1,31 +1,25 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import CustomError from '../errors/custom.error';
 
-/**
- * Middleware for error handling
- * @param error The error object
- * @param req The express request
- * @param res The Express response
- * @param next
- * @returns
- */
-export const errorMiddleware = (
-  error: ErrorRequestHandler,
+
+export const errorMiddleware: ErrorRequestHandler = (
+  error: Error,
   req: Request,
   res: Response,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
   if (error instanceof CustomError) {
-    if (typeof error.statusCode === 'number') {
-      res.status(error.statusCode).json({
+    
+      res.status(error.statusCode as number).json({
         message: error.message,
         errors: error.errors,
+        stack: error.stack,
       });
 
       return;
-    }
+    
   }
 
-  res.status(500).send('Internal server error');
+  res.status(500).send('Internal server error: ' + error.stack);
 };
