9935952b017b971830f93eab867d8f6729918e29
Rui Pedro Dias <rui.dias@commercetools.com>
2024-07-16 11:53:56 +0100

    fix(templates): fix error middleware handling

--- application-templates/javascript/job/src/middleware/error.middleware.js
+++ application-templates/javascript/job/src/middleware/error.middleware.js
@@ -1,17 +1,18 @@
 import CustomError from '../errors/custom.error.js';
 
-export const errorMiddleware = (error, _req, res, _next) => {
+export const errorMiddleware = (error, _req, res) => {
   if (error instanceof CustomError) {
     if (typeof error.statusCode === 'number') {
       res.status(error.statusCode).json({
         message: error.message,
         errors: error.errors,
-        stack: error.stack
+        stack: process.env.NODE_ENV === 'development' ? error.stack : undefined,
       });
 
       return;
     }
   }
 
-  res.status(500).send('Internal server error: ' + error.stack);
+  res.status(500).send('Internal server error: ' + process.env.NODE_ENV === 'development' ? error.stack : undefined);
 };
+
--- application-templates/typescript/event/src/middleware/error.middleware.ts
+++ application-templates/typescript/event/src/middleware/error.middleware.ts
@@ -1,25 +1,20 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, Request, Response } from 'express';
 import CustomError from '../errors/custom.error';
-
 
 export const errorMiddleware: ErrorRequestHandler = (
   error: Error,
-  req: Request,
+  _: Request,
   res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  next: NextFunction
 ) => {
   if (error instanceof CustomError) {
-    
-      res.status(error.statusCode as number).json({
-        message: error.message,
-        errors: error.errors,
-        stack: error.stack,
-      });
+    res.status(error.statusCode as number).json({
+      message: error.message,
+      errors: error.errors,
+      stack: process.env.NODE_ENV === 'development' ? error.stack : undefined,
+    });
 
-      return;
-    
+    return;
   }
 
-  res.status(500).send('Internal server error: ' + error.stack);
+  res.status(500).send('Internal server error: ' + process.env.NODE_ENV === 'development' ? error.stack : undefined);
 };
