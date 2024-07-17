286207816fc7b565f4bec1a3a6a029592312b4ca
Rui Pedro Dias <rui.dias@commercetools.com>
2024-07-17 10:12:42 +0100

    fix(templates): fix asset configuration with new suggestions

--- application-templates/typescript/assets/vite.config.js
+++ application-templates/typescript/assets/vite.config.js
@@ -1,12 +1,11 @@
 import { resolve, dirname } from 'path';
 import { defineConfig } from 'vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [cssInjectedByJsPlugin()],
   build: {
     outDir: resolve(__dirname, 'public'),
   },
+  publicDir: resolve(__dirname, 'static'),
 });
